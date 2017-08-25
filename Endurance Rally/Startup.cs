f8dfd835e8fd0cbd8569dbe02fae1e5df388f659
Dimitar Filipov <dfilipov94@gmail.com>
2017-08-25 20:02:35 +0300

    Added Endurance Rally

---
+++ Endurance Rally/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endurance_Rally
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            List<string> drivers = Console.ReadLine().Split(' ').ToList();
+            List<double> zoneFuel = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            List<int> checkpoints = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            foreach (var driver in drivers)
+            {
+                int reachedIndex = 0;
+                double fuel = driver[0];
+                bool hasFuel = true;
+                for (int i = 0; i < zoneFuel.Count; i++)
+                {
+                    if (checkpoints.Contains(i))
+                    {
+                        fuel += zoneFuel[i];
+                    }
+                    else
+                    {
+                        fuel -= zoneFuel[i];
+                    }
+
+                    if (fuel <= 0)
+                    {
+                        hasFuel = false;
+                        reachedIndex = i;
+                        break;
+                    }
+                }
+                if (hasFuel)
+                {
+                    Console.WriteLine($"{driver} - fuel left {fuel:F2}");
+                }
+                else
+                {
+                    Console.WriteLine($"{driver} reached {reachedIndex}");
+                }
+
+            }
+        }
+    }
+}
